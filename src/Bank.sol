941ed03ee1f2bf5d914ed8df75e3f0e4b3fff415
gmh5225 <2315157@qq.com>
2024-11-05 12:27:03 +0800

    update

--- src/Bank.sol
+++ src/Bank.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "forge-std/console2.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
@@ -17,6 +18,8 @@ contract Bank {
 
     constructor() {
         for (uint256 i = 0; i < 11; i++) {
+            uint64 startTime = uint64(block.timestamp * 2 - i);
+            console2.log("startTime: ", startTime);
             _locks.push(LockInfo(address(uint160(i + 1)), uint64(block.timestamp * 2 - i), 1e18 * (i + 1)));
         }
     }
